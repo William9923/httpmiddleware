b10f434805ee083498aad471dd0a4cda2edb4fbf
William9923 <williamong1400@gmail.com>
2022-03-01 20:12:19 +0700

    feat: add example

--- example/app.go
+++ example/app.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/William9923/httpmiddleware"
 	"github.com/julienschmidt/httprouter"
 )
 
@@ -20,9 +21,31 @@ func Index(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	fmt.Fprintf(w, "Hello, %s! Welcome to sample web app for httpmiddleware demo...\n", name)
 }
 
+func Logging(h httprouter.Handle) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+		logger := log.Default()
+		logger.Println("start http request...")
+		h(w, r, ps)
+	}
+}
+
+func Authentication(fn httprouter.Handle) httprouter.Handle {
+	return func(w http.ResponseWriter, req *http.Request, p httprouter.Params) {
+
+		ctx := req.Context()
+
+		req = req.WithContext(ctx)
+		fn(w, req, p)
+	}
+}
+
 func main() {
+
+	middlewares := httpmiddleware.New()
+	middlewares.Use(Logging)
+
 	router := httprouter.New()
-	router.GET("/", Index)
+	router.GET("/", middlewares.Wrap(Index))
 	router.GET("/ping", ping)
 
 	log.Fatal(http.ListenAndServe(":8080", router))
